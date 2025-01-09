0ff0db402270085bb6a6c501027dfb0dde84219a
SchadenKai <kairustecsonpersonal@gmail.com>
2025-01-09 12:55:20 +0800

    load OPENAI API key in application startup

--- api_server/app/main.py
+++ api_server/app/main.py
@@ -1,10 +1,14 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+import openai
 from starlette.middleware.cors import CORSMiddleware
-from app.routers.chat import router as chat_router
+from app.api.v1.endpoints.chat import router as chat_router
+from app.core.config import Settings
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    print("Loading OPENAI API key")
+    openai.api_key = Settings().openai_key
     print("App started")
     yield
 
